9ef62b8d43ce203b7c637536c95d983ceb0b33e7
Cristian Rojas <u6000290@unimilitar.edu.co>
2022-05-05 13:14:23 -0500

    primer avance

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+
+let slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider-section");
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+let btnLeft = document.querySelector("#btn-left");
+let btnRight = document.querySelector("#slider-right");
+
+const next = () => {
+    let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(() => {
+      slider.style.transition = "none";
+      slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+      slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+const prev = () => {
+    let sliderSection = document.querySelectorAll(".slider-section");
+    let sliderSectionLast = sliderSection[sliderSection.length - 1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(() => {
+      slider.style.transition = "none";
+      slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+      slider.style.marginLeft = "-100%";
+    }, 500);
+}
